88892e40faf9ff8a8a24beeaabbaf227e7bd283c
Willian Lima <william_slayer@hotmail.com>
2023-01-04 08:40:42 -0300

    Pokedex finalizada

--- src/app/feactures/pokemon/pokemon.component.ts
+++ src/app/feactures/pokemon/pokemon.component.ts
@@ -1,24 +1,29 @@
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { FormComponent } from './../components/form/form.component';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { PokemonModel } from './../../shared/models/pokemon-model';
 import { PokedexFirestoreService } from './../../core/services/pokedex-firestore.service';
-import { Component, Inject, OnInit } from '@angular/core';
-import { tap,filter } from 'rxjs/operators';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { tap,filter, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-pokemon',
   templateUrl: './pokemon.component.html',
   styleUrls: ['./pokemon.component.css']
 })
-export class PokemonComponent implements OnInit {
+export class PokemonComponent implements OnInit, OnDestroy {
   pokemon$:Observable<PokemonModel[]>;
+  destroyed$ = new Subject<void>();
   selectedPokemon:PokemonModel;
 
   constructor(
     private pokedexService:PokedexFirestoreService,
     private dialog:MatDialog
   ) { }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+  }
 
   ngOnInit(): void {
     this.pokemon$ = this.pokedexService.getAll();
@@ -32,7 +37,8 @@ export class PokemonComponent implements OnInit {
 
     dialogRef
       .afterClosed()
-      .pipe(filter(Boolean),tap((res:any)=> this.pokedexService.create(res))
+      .pipe(filter(Boolean),tap((res:any)=> this.pokedexService.create(res)),
+      takeUntil(this.destroyed$)
       )
       .subscribe();
   }
@@ -47,7 +53,8 @@ export class PokemonComponent implements OnInit {
       .pipe(
         filter(Boolean),
         tap((res:any) => this.pokedexService.update(res)),
-        tap((res) => this.selectPokemon(res))
+        tap((res) => this.selectPokemon(res)),
+        takeUntil(this.destroyed$)
       )
       .subscribe();
   }
